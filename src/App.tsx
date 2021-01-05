db8efb0488b762783544d91c03ec45b324f20606
najmaslo <maseu@protonmail.com>
2021-01-05 20:00:22 +0100

    add emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
 import React from 'react';
 
 export const App: React.FC = () => {
   return (
-    <div className="App">
+    <div css={css(
+      {
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }
+    )}>
      Hello
     </div>
   );
